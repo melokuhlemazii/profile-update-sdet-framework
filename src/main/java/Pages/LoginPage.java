46248742e0ebe3a442d654a6671342777c62391a
melokuhlemazii <melomazibuko8@gmail.com>
2026-09-10 22:19:01 +0200

    Created Pipeline

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,4 +1,7 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    static WebDriver driver;
 }
--- src/test/java/StepDefinitions/StepsDefinitions.java
+++ src/test/java/StepDefinitions/StepsDefinitions.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 public class StepsDefinitions extends Base {
@@ -7,6 +8,11 @@ public class StepsDefinitions extends Base {
     @Given("i am on the login page")
     public void i_am_on_the_login_page() {
         homePage.clickLoginButton();
+    }
+
+    @And("i enter email (.*)$")
+    public void i_enter_email(String email){
+        loginPage.enterEmail(email);
     }
 }
 
